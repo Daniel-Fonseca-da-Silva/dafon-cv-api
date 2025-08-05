c5f6ce6ba8c930bdf88e00efd4b4011e395428e5
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-08-05 09:56:37 +0100

    fix: added another fields

--- internal/models/curriculums.go
+++ internal/models/curriculums.go
@@ -11,12 +11,16 @@ type Curriculums struct {
 	ID                uuid.UUID      `json:"id" gorm:"type:char(36);primary_key;default:(UUID());table:curriculums"`
 	FullName          string         `json:"full_name" gorm:"size:255;not null"`
 	Email             string         `json:"email" gorm:"size:255;not null"`
+	Phone             string         `json:"phone" gorm:"size:20;not null"`
 	DriverLicense     string         `json:"driver_license" gorm:"size:255"`
-	AboutMe           string         `json:"about_me" gorm:"type:text"`
+	Intro             string         `json:"intro" gorm:"type:text;not null"`
+	Technologies      string         `json:"technologies" gorm:"type:text;not null"`
 	DateDisponibility time.Time      `json:"date_disponibility" gorm:"type:date"`
 	Languages         string         `json:"languages" gorm:"type:text;not null"`
 	LevelEducation    string         `json:"level_education" gorm:"size:255;not null"`
-	CompanyInfo       string         `json:"company_info" gorm:"type:text"`
+	Courses           string         `json:"courses" gorm:"type:text"`
+	SocialLinks       string         `json:"social_links" gorm:"type:text"`
+	JobDescription    string         `json:"job_description" gorm:"type:text"`
 	Works             []Work         `json:"works" gorm:"foreignKey:CurriculumID"`
 	CreatedAt         time.Time      `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt         time.Time      `json:"updated_at" gorm:"autoUpdateTime"`
