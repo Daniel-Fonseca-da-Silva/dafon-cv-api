6d7294cf2ca8f2b482f67e6255de4e4127bcf871
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-08-10 01:52:26 +0100

    fix: fixed curriculum dto with missing fields

--- internal/dto/curriculum_dto.go
+++ internal/dto/curriculum_dto.go
@@ -10,11 +10,15 @@ import (
 type CreateCurriculumRequest struct {
 	FullName          string              `json:"full_name" binding:"required,min=5,max=50"`
 	Email             string              `json:"email" binding:"required,email"`
+	Phone             string              `json:"phone" binding:"required"`
 	DriverLicense     string              `json:"driver_license" binding:"required"`
 	Intro             string              `json:"intro" binding:"required"`
+	Technologies      string              `json:"technologies" binding:"required"`
 	DateDisponibility *time.Time          `json:"date_disponibility"`
 	Languages         string              `json:"languages" binding:"required"`
 	LevelEducation    string              `json:"level_education" binding:"required,min=5"`
+	Courses           string              `json:"courses"`
+	SocialLinks       string              `json:"social_links"`
 	JobDescription    string              `json:"job_description"`
 	Works             []CreateWorkRequest `json:"works"`
 }
