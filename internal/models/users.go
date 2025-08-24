6ba01710b667ea452fb15486cd1579bb7ac4b36d
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-08-24 21:49:08 +0100

    fix: added the gorm model of user

--- internal/models/users.go
+++ internal/models/users.go
@@ -1,21 +1,18 @@
 package models
 
 import (
-	"time"
-
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	ID          uuid.UUID      `json:"id" gorm:"type:char(36);primary_key;default:(UUID());table:user"`
-	Name        string         `json:"name" gorm:"size:255;not null"`
-	Email       string         `json:"email" gorm:"size:255;unique;not null"`
-	Password    string         `json:"-" gorm:"size:255;not null"` // "-" hides password from JSON
-	CreatedAt   time.Time      `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt   time.Time      `json:"updated_at" gorm:"autoUpdateTime"`
-	DeletedAt   gorm.DeletedAt `json:"-" gorm:"index"`
-	Curriculums []Curriculums  `json:"curriculums,omitempty" gorm:"foreignKey:UserID"`
+	gorm.Model
+	ID            uuid.UUID      `json:"id" gorm:"type:char(36);primary_key;default:(UUID());table:user"`
+	Name          string         `json:"name" gorm:"size:255;not null"`
+	Email         string         `json:"email" gorm:"size:255;unique;not null"`
+	Password      string         `json:"-" gorm:"size:255;not null"` // "-" hides password from JSON
+	Curriculums   []Curriculums  `json:"curriculums,omitempty" gorm:"foreignKey:UserID"`
+	Configuration *Configuration `json:"configuration,omitempty" gorm:"foreignKey:UserID"`
 }
 
 // BeforeCreate will set a UUID rather than numeric ID
