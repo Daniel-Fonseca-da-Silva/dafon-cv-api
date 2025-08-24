b58524fd60b95641e8f10f20acc56303615a173c
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-08-24 21:43:29 +0100

    feat: created the model of configuration

---
+++ internal/models/configurations.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Configuration struct {
+	gorm.Model
+	ID            uuid.UUID `json:"id" gorm:"type:char(36);primary_key;default:(UUID());table:configuration"`
+	UserID        uuid.UUID `json:"user_id" gorm:"type:char(36);not null;uniqueIndex"`
+	User          User      `json:"user,omitempty" gorm:"foreignKey:UserID"`
+	Language      string    `json:"language" gorm:"size:255;not null;default:'en-us'"`
+	Newsletter    bool      `json:"newsletter" gorm:"not null;default:false"`
+	ReceiveEmails bool      `json:"receive_emails" gorm:"not null;default:false"`
+}
+
+// BeforeCreate will set a UUID rather than numeric ID
+func (c *Configuration) BeforeCreate(tx *gorm.DB) error {
+	if c.ID == uuid.Nil {
+		c.ID = uuid.New()
+	}
+	return nil
+}
