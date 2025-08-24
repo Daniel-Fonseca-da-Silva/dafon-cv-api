742e3ba8ef2586284590606fce01bc95caf06633
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-08-24 21:48:54 +0100

    fix: added the gorm model of curriculum

--- internal/models/curriculums.go
+++ internal/models/curriculums.go
@@ -8,25 +8,23 @@ import (
 )
 
 type Curriculums struct {
-	ID                uuid.UUID      `json:"id" gorm:"type:char(36);primary_key;default:(UUID());table:curriculums"`
-	FullName          string         `json:"full_name" gorm:"size:255;not null"`
-	Email             string         `json:"email" gorm:"size:255;not null"`
-	Phone             string         `json:"phone" gorm:"size:20;not null"`
-	DriverLicense     string         `json:"driver_license" gorm:"size:255"`
-	Intro             string         `json:"intro" gorm:"type:text;not null"`
-	Technologies      string         `json:"technologies" gorm:"type:text;not null"`
-	DateDisponibility time.Time      `json:"date_disponibility" gorm:"type:date"`
-	Languages         string         `json:"languages" gorm:"type:text;not null"`
-	LevelEducation    string         `json:"level_education" gorm:"size:255;not null"`
-	Courses           string         `json:"courses" gorm:"type:text"`
-	SocialLinks       string         `json:"social_links" gorm:"type:text"`
-	JobDescription    string         `json:"job_description" gorm:"type:text"`
-	Works             []Work         `json:"works" gorm:"foreignKey:CurriculumID"`
-	CreatedAt         time.Time      `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt         time.Time      `json:"updated_at" gorm:"autoUpdateTime"`
-	DeletedAt         gorm.DeletedAt `json:"-" gorm:"index"`
-	UserID            uuid.UUID      `json:"user_id" gorm:"type:char(36);not null;index"`
-	User              User           `json:"-" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:UserID;references:ID"`
+	ID uuid.UUID `json:"id" gorm:"type:char(36);primary_key;default:(UUID());table:curriculums"`
+	gorm.Model
+	FullName          string    `json:"full_name" gorm:"size:255;not null"`
+	Email             string    `json:"email" gorm:"size:255;not null"`
+	Phone             string    `json:"phone" gorm:"size:20;not null"`
+	DriverLicense     string    `json:"driver_license" gorm:"size:255"`
+	Intro             string    `json:"intro" gorm:"type:text;not null"`
+	Technologies      string    `json:"technologies" gorm:"type:text;not null"`
+	DateDisponibility time.Time `json:"date_disponibility" gorm:"type:date"`
+	Languages         string    `json:"languages" gorm:"type:text;not null"`
+	LevelEducation    string    `json:"level_education" gorm:"size:255;not null"`
+	Courses           string    `json:"courses" gorm:"type:text"`
+	SocialLinks       string    `json:"social_links" gorm:"type:text"`
+	JobDescription    string    `json:"job_description" gorm:"type:text"`
+	Works             []Work    `json:"works" gorm:"foreignKey:CurriculumID"`
+	UserID            uuid.UUID `json:"user_id" gorm:"type:char(36);not null;index"`
+	User              User      `json:"-" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:UserID;references:ID"`
 }
 
 // BeforeCreate will set a UUID rather than numeric ID
