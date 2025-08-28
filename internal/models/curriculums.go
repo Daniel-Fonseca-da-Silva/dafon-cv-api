061a15047db6cc307fbd4801ba8d81d38553d530
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-08-28 12:04:34 +0100

    fix: fixed curriculums fields

--- internal/models/curriculums.go
+++ internal/models/curriculums.go
@@ -10,20 +10,20 @@ import (
 type Curriculums struct {
 	ID uuid.UUID `json:"id" gorm:"type:char(36);primary_key;default:(UUID());table:curriculums"`
 	gorm.Model
-	FullName          string    `json:"full_name" gorm:"size:255;not null"`
-	Email             string    `json:"email" gorm:"size:255;not null"`
-	Phone             string    `json:"phone" gorm:"size:20;not null"`
+	FullName          string    `json:"full_name" gorm:"size:255;not null" validate:"required"`
+	Email             string    `json:"email" gorm:"size:255;not null" validate:"required,email"`
+	Phone             string    `json:"phone" gorm:"size:20;not null" validate:"required"`
 	DriverLicense     string    `json:"driver_license" gorm:"size:255"`
-	Intro             string    `json:"intro" gorm:"type:text;not null"`
-	Technologies      string    `json:"technologies" gorm:"type:text;not null"`
+	Intro             string    `json:"intro" gorm:"type:text;not null" validate:"required"`
+	Technologies      string    `json:"technologies" gorm:"type:text;not null" validate:"required"`
 	DateDisponibility time.Time `json:"date_disponibility" gorm:"type:date"`
-	Languages         string    `json:"languages" gorm:"type:text;not null"`
-	LevelEducation    string    `json:"level_education" gorm:"size:255;not null"`
+	Languages         string    `json:"languages" gorm:"type:text;not null" validate:"required"`
+	LevelEducation    string    `json:"level_education" gorm:"size:255;not null" validate:"required"`
 	Courses           string    `json:"courses" gorm:"type:text"`
-	SocialLinks       string    `json:"social_links" gorm:"type:text"`
+	SocialLinks       string    `json:"social_links" gorm:"size:100" validate:"url"`
 	JobDescription    string    `json:"job_description" gorm:"type:text"`
 	Works             []Work    `json:"works" gorm:"foreignKey:CurriculumID"`
-	UserID            uuid.UUID `json:"user_id" gorm:"type:char(36);not null;index"`
+	UserID            uuid.UUID `json:"user_id" gorm:"type:char(36);not null;index" validate:"required"`
 	User              User      `json:"-" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:UserID;references:ID"`
 }
 
